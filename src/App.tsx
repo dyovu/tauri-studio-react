167f491821a5200bb94165cda018df7a6d2d5e6e
Soma Sekine <a8121151@aoyama.jp>
2025-03-22 14:11:27 +0900

    画面収録画面にボタン追加

--- src/App.tsx
+++ src/App.tsx
@@ -3,22 +3,8 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import ScreenShot from "./pages/ScreenShot";
 import Home from "./pages/Home";
 import ScreenMovie from "./pages/ScreenMovie";
-import { useState } from "react";
 
 function App() {
-    const [isRecording, setIsRecording] = useState(false);
-
-    const startRecording = async () => {
-        setIsRecording(true);
-
-        // 録画開始時の処理....
-    };
-    const stopRecording = async () => {
-        setIsRecording(false);
-
-        // 録画終了時の処理....
-    };
-
     return (
         <>
             <BrowserRouter>
@@ -28,25 +14,6 @@ function App() {
                     <Route path="/screen-movie" element={<ScreenMovie />} />
                 </Routes>
             </BrowserRouter>
-
-            <div className="grid place-items-center h-screen">
-                {!isRecording && (
-                    <button
-                        onClick={startRecording}
-                        className="text-4xl bg-slate-400 p-3 rounded-md"
-                    >
-                        Start Recording
-                    </button>
-                )}
-                {isRecording && (
-                    <button
-                        onClick={stopRecording}
-                        className="text-4xl bg-red-400 p-3 rounded-md"
-                    >
-                        Stop Recording
-                    </button>
-                )}
-            </div>
         </>
     );
 }
