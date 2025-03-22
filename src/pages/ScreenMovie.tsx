ff03b7bc128b9596f40478c39081d956cebe6305
Soma Sekine <a8121151@aoyama.jp>
2025-03-22 21:55:19 +0900

    録画プレビュー機能追加

--- src/pages/ScreenMovie.tsx
+++ src/pages/ScreenMovie.tsx
@@ -1,41 +1,53 @@
-import { invoke } from "@tauri-apps/api/core";
-import { useState } from "react";
+import { convertFileSrc, invoke } from "@tauri-apps/api/core";
+import { join } from "@tauri-apps/api/path";
+import { useState, useRef } from "react";
 
 export default function ScreenMovie() {
-    const [isRecording, setIsRecording] = useState(false);
+  const [isRecording, setIsRecording] = useState(false);
+  const [videoUrl, setVideoUrl] = useState<string>();
+  const filepath = useRef<string>();
 
-    const startRecording = async () => {
-        setIsRecording(true);
+  const startRecording = async () => {
+    setIsRecording(true);
 
-        await invoke("start_recording");
-    };
-    const stopRecording = async () => {
-        setIsRecording(false);
+    const videoPath = await invoke<string>("start_recording");
+    filepath.current = convertFileSrc(await join("/private", videoPath));
+  };
+  const stopRecording = async () => {
+    setIsRecording(false);
 
-        await invoke("stop_recording");
-    };
-    return (
-        <>
-            <div>ScreenMovie</div>
+    await invoke("stop_recording");
+    setVideoUrl(filepath.current);
+  };
 
-            <div className="grid place-items-center h-screen">
-                {!isRecording && (
-                    <button
-                        onClick={startRecording}
-                        className="text-4xl bg-slate-400 p-3 rounded-md"
-                    >
-                        Start Recording
-                    </button>
-                )}
-                {isRecording && (
-                    <button
-                        onClick={stopRecording}
-                        className="text-4xl bg-red-400 p-3 rounded-md"
-                    >
-                        Stop Recording
-                    </button>
-                )}
-            </div>
-        </>
-    );
+  return (
+    <>
+      <div>ScreenMovie</div>
+
+      <div className="grid place-items-center h-screen">
+        {!isRecording && (
+          <button
+            onClick={startRecording}
+            className="text-4xl bg-slate-400 p-3 rounded-md"
+          >
+            Start Recording
+          </button>
+        )}
+        {isRecording && (
+          <button
+            onClick={stopRecording}
+            className="text-4xl bg-red-400 p-3 rounded-md"
+          >
+            Stop Recording
+          </button>
+        )}
+      </div>
+
+      {videoUrl && (
+        <video src={videoUrl} controls width="800" className="mt-4">
+          お使いのブラウザは video タグに対応していません。
+        </video>
+      )}
+    </>
+  );
 }
